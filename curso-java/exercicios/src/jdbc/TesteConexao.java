5b0d905df8ceeffd18d997abb25117ca8d161184
AquilesDias <kilesax@gmail.com>
2021-01-13 15:24:10 -0300

    JDBC - Abrindo conexão

---
+++ curso-java/exercicios/src/jdbc/TesteConexao.java
@@ -0,0 +1,20 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TesteConexao {
+
+    public static void main(String[] args) throws SQLException {
+	final String url = "jdbc:mysql://localhost:3306/curso_bd?verifyServerCertificate=false&useSSL=true";
+	final String usuario = "root";
+	final String senha = "admin";
+
+	Connection conexao = DriverManager.getConnection(url, usuario, senha);
+
+	System.out.println("Conectado com Sucesso!");
+	conexao.close();
+    }
+
+}
